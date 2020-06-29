7d59da19c132ccd2a4c381368f3fed19f9f1d343
wb-zj268791 <wb-zj268791@alibaba-inc.com>
2020-06-29 16:22:03 +0800

    rm

---
+++ components/SimpleAuth.js
@@ -0,0 +1,24 @@
+function Auth() {
+    const storage = window.localStorage.getItem("auth");
+    if (undefined === storage) {
+        return false;
+    }
+    return storage === '1'
+}
+
+export function HandleAuth() {
+    const auth = Auth()
+    if (!auth) {
+        if (window.location.pathname === '/ant') {
+            return;
+        } else {
+            window.location.href = "/ant";
+        }
+    } else {
+        if (window.location.pathname === '/') {
+            return;
+        } else {
+            window.location.href = "/";
+        }
+    }
+}
