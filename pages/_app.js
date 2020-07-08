d8ff506c2916f448ab23b586e44a4419969f903b
helixcs <zhangjian12424@gmail.com>
2020-07-08 08:18:44 +0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import App from 'next/app';
 import 'antd/dist/antd.css';
 import '../static/css/google_fonts_family.css'
-// import '../static/css/css2.css'
+import '../static/css/css2.css'
 import '../css/g.css'
 
 export default function MyApp({Component,pageProps}){
